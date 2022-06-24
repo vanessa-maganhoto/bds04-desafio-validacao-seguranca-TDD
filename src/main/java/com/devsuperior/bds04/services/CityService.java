85f3b3686a20bf21fc40de0095391cf9309ca0e5
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-06-24 16:53:24 -0300

    Implementação das funcionalidades para contemplar os testes de CityControllerIT

---
+++ src/main/java/com/devsuperior/bds04/services/CityService.java
@@ -0,0 +1,34 @@
+package com.devsuperior.bds04.services;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.entities.City;
+import com.devsuperior.bds04.repositories.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        List<City> list = cityRepository.findAll(Sort.by("name"));
+        return list.stream().map(x -> new CityDTO(x)).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto){
+        City newCity = new City();
+        newCity.setName(dto.getName());
+        newCity = cityRepository.save(newCity);
+        return  new CityDTO(newCity);
+    }
+
+}
